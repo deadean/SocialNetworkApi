38e3613393dff838d10dbe64e8c46fd2c45a8632
coolhunter33 <bogdanmalenuk@gmail.com>
2015-08-27 15:21:09 +0300

    #upd1 with MVVM

---
+++ VK/sample1/vk2/VVms/Views/MainWindowView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace vk2.VVms.Views
+{
+	/// <summary>
+	/// Description for MainWindowView.
+	/// </summary>
+	public partial class MainWindowView : Window
+	{
+		public MainWindowView()
+		{
+			InitializeComponent();
+		}
+	}
+}
