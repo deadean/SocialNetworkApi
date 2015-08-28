448b0981d537129759dffb7af025608b17095cbf
deadean <deadean@yandex.ru>
2015-08-28 18:10:26 +0300

    #added OAuth in main project

---
+++ XamarinSocialNetwork/XamarinSocialApp/Services/XamarinSocialApp.Services.UI/Interfaces/Web/OAuth/IOAuthService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinSocialApp.Services.UI.Interfaces.Web.OAuth
+{
+	public interface IOAuthService
+	{
+		Task<bool> Login();
+	}
+}
--- XamarinSocialNetwork/XamarinSocialApp/UI/Common/XamarinSocialApp.UI.Common.VVm/Implementations/ViewModels/MainPageVm.cs
+++ XamarinSocialNetwork/XamarinSocialApp/UI/Common/XamarinSocialApp.UI.Common.VVm/Implementations/ViewModels/MainPageVm.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Common.MVVM.Library;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using XamarinSocialApp.Services.UI.Interfaces.Web.OAuth;
 using XamarinSocialApp.UI.Common.Implementations.Bases;
 using XamarinSocialApp.UI.Common.Interfaces.ViewModels;
 
@@ -10,5 +13,74 @@ namespace XamarinSocialApp.UI.Common.VVm.Implementations.ViewModels
 {
 	public class MainPageVm : AdvancedPageViewModelBase, IMainPageVm
 	{
+
+		#region Fields
+
+		#endregion
+
+		#region Services
+
+		private readonly IOAuthService modOAuthService;
+
+		#endregion
+
+		#region Properties
+
+		#endregion
+
+		#region Commands
+
+		public ICommand LoginCommand
+		{
+			get;
+			private set;
+		}
+
+		public ICommand LoadMessagesCommand
+		{
+			get;
+			private set;
+		}
+		
+		#endregion
+
+		#region Ctor
+
+		public MainPageVm(IOAuthService oAuthService)
+		{
+			modOAuthService = oAuthService;
+			LoginCommand = new AsyncCommand(OnLoginCommand);
+		}
+
+		#endregion
+
+		#region Public Methods
+
+		#endregion
+
+		#region Private Methods
+
+		#endregion
+
+		#region Protected Methods
+
+		#endregion
+
+		#region Command Execute Handlers
+
+		private async Task OnLoginCommand()
+		{
+			try
+			{
+				await modOAuthService.Login();
+			}
+			catch (Exception ex)
+			{
+				//this.GetLog().Write(ex);
+			}
+		}
+		
+		#endregion
+
 	}
 }
