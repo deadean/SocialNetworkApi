2422432e02844b1394328ea1418b66a33304526e
deadean <deadean@yandex.ru>
2015-09-04 18:07:08 +0300

    #updated WinRT support

---
+++ XamarinSocialNetwork/XamarinSocialApp/XamarinSocialApp/XamarinSocialApp.WinRT.Tablet/Implementations/Services/OAuth/OAuthService.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Json;
+using Windows.Security.Authentication.Web;
+using Windows.Web.Http;
+using Xamarin.Forms;
+using XamarinSocialApp.Data.Common.Enums;
+using XamarinSocialApp.Data.Interfaces.Entities.Database;
+using XamarinSocialApp.Services.UI.Interfaces.Web.OAuth;
+using XamarinSocialApp.UI.Data.Implementations.Entities.Databases;
+using XamarinSocialApp.WinRT.Tablet.Implementations.Services.OAuth;
+
+[assembly: Dependency(typeof(OAuthService))]
+
+namespace XamarinSocialApp.WinRT.Tablet.Implementations.Services.OAuth
+{
+	public class OAuthService : IOAuthService
+	{
+
+		#region Services
+
+		#endregion
+
+		#region Fields
+
+		#endregion
+
+		#region Properties
+
+		#endregion
+
+		#region Ctor
+
+		#endregion
+
+		#region Commands
+
+		#endregion
+
+		#region Commands Execute Handlers
+
+		#endregion
+
+		#region Public Methods
+
+		public async Task<IUser> Login(enSocialNetwork socialNetwork)
+		{
+			IUser user = null;
+			try
+			{
+				String vkUrl =
+					String.Format("https://oauth.vk.com/authorize?client_id={0}&scope={1}&response_type=token"
+					, 5042701, "offline,messages,friends");
+				String vkCallBackUrl = String.Format("https://oauth.vk.com/blank.html");
+
+				System.Uri StartUri = new Uri(vkUrl);
+				System.Uri EndUri = new Uri(vkCallBackUrl);
+
+				WebAuthenticationResult response = 
+					await WebAuthenticationBroker.AuthenticateAsync(WebAuthenticationOptions.None,StartUri,EndUri);
+
+				if (response.ResponseStatus == WebAuthenticationStatus.Success)
+				{
+					user = await GetVkUser(response.ResponseData.ToString());
+				}
+
+			}
+			catch (Exception ex)
+			{
+
+			}
+
+			return user;
+		}
+
+		public async Task<IEnumerable<IDialog>> GetDialogs(IUser user, enSocialNetwork socialNetwork)
+		{
+			IList<IDialog> dialogs = null;
+			try
+			{
+				String url =
+					String.Format("https://api.vk.com/method/messages.getDialogs?&count={0}&access_token={1}"
+					, 100.ToString(), user.SerializeInfo);
+
+				HttpClient httpClient = new HttpClient();
+				string response =
+					await httpClient.GetStringAsync(new Uri(url));
+
+				string[] responseString = response.Split('[');
+				string[] responseDialogsArray = responseString[1].Split('{');
+
+				dialogs = new List<IDialog>();
+				foreach (var item in responseDialogsArray)
+				{
+					try
+					{
+						string uid = item.Split(',')[3].Split(':')[1];
+						string message = String.Empty;
+						try
+						{
+							message = item.Split(',')[6].Split(':')[1];
+						}
+						catch (Exception ex)
+						{
+							message = item.Split(',')[6].Split('\"')[0].Trim();
+						}
+
+						IUser friend = new User() { Uid = uid, SerializeInfo = user.SerializeInfo };
+						IMessage friendMessage = new Message() { Content = message };
+						IDialog dialog = new Dialog(friend, new List<IMessage>() { friendMessage });
+						dialogs.Add(dialog);
+					}
+					catch (Exception ex)
+					{
+
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+
+			}
+
+			return dialogs;
+		}
+
+		public Task<IEnumerable<IUser>> GetUserFriends(IUser user, enSocialNetwork enSocialNetwork)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IDialog> GetDialogWithFriend(IUser user, enSocialNetwork socialNetwork, IUser friend)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<IUser> GetUserInfoRequest(IUser user, enSocialNetwork enSocialNetwork)
+		{
+			try
+			{
+				HttpClient httpClient = new HttpClient();
+				string urlGetUser =
+					String.Format("https://api.vk.com/method/users.get?token={0}&user_ids={1}",
+					user.SerializeInfo, user.Uid
+					);
+
+				string response =
+					await httpClient.GetStringAsync(new Uri(urlGetUser));
+
+				string firstName = response.Split('[')[1].Split(',')[1].Split('\"')[3];
+				string lastName = response.Split('[')[1].Split(',')[2].Split('\"')[3];
+
+				return new User() { FirstName = firstName, LastName = lastName, Uid = user.Uid, SerializeInfo = user.SerializeInfo };
+			}
+			catch (Exception ex)
+			{
+
+			}
+			return null;
+		}
+
+		public Task<bool> SendMessage(IUser user, IUser friend, string Message, enSocialNetwork enSocialNetwork)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task RegisterInLongPoolServer(IUser user)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		private async Task<IUser> GetVkUser(string webAuthResultResponseData)
+		{
+			string responseData = webAuthResultResponseData.Substring(webAuthResultResponseData.IndexOf("access_token"));
+			String[] keyValPairs = responseData.Split('&');
+			string access_token = null;
+			string expires_in = null;
+			string uid = null;
+			for (int i = 0; i < keyValPairs.Length; i++)
+			{
+				String[] splits = keyValPairs[i].Split('=');
+				switch (splits[0])
+				{
+					case "access_token":
+						access_token = splits[1];
+						break;
+					case "expires_in":
+						expires_in = splits[1];
+						break;
+					case "user_id":
+						uid = splits[1];
+						break;
+				}
+			}
+
+			HttpClient httpClient = new HttpClient();
+			string urlGetUser = 
+				String.Format("https://api.vk.com/method/users.get?token={0}&user_ids={1}",
+				access_token, uid
+				);
+			string response =
+				await httpClient.GetStringAsync(new Uri(urlGetUser));
+
+			string firstName = response.Split('[')[1].Split(',')[1].Split('\"')[3];
+			string lastName = response.Split('[')[1].Split(',')[2].Split('\"')[3];
+
+			return new User() { FirstName = firstName, LastName = lastName, Uid = uid, SerializeInfo = access_token };
+		}
+
+		#endregion
+
+		#region Protected Methods
+
+		#endregion
+		
+	}
+}
---
+++ XamarinSocialNetwork/XamarinSocialApp/XamarinSocialApp/XamarinSocialApp.WinRT.Tablet/Implementations/Services/SQLiteConnector/WinRtTablet_SQLiteConnection.cs
@@ -0,0 +1,59 @@
+﻿using SQLite.Net;
+using SQLite.Net.Async;
+using SQLite.Net.Platform.WinRT;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Xamarin.Forms;
+using XamarinSocialApp.UI.Services.Implementations.SQLiteConnector;
+using XamarinSocialApp.UI.Services.Interfaces.SQLiteConnector;
+using XamarinSocialApp.WinRT.Tablet.Implementations.Services.SQLiteConnector;
+
+[assembly: Dependency(typeof(WinRtTablet_SQLiteConnection))]
+
+namespace XamarinSocialApp.WinRT.Tablet.Implementations.Services.SQLiteConnector
+{
+	public class WinRtTablet_SQLiteConnection : ISQLiteConnection
+	{
+		public async Task<ConnectionInfo<SQLiteAsyncConnection>> GetConnection()
+		{
+			ConnectionInfo<SQLiteAsyncConnection> connectionInfo = new ConnectionInfo<SQLiteAsyncConnection>();
+			connectionInfo.IsInitializedDbStructure = true;
+
+			var sqliteFilename = XamarinSocialApp.UI.Common.Implementations.Constants.Constants.Configuration.csLocalDbFileName;
+			string path = Path.Combine(ApplicationData.Current.LocalFolder.Path, sqliteFilename);
+
+			if (!await FileExists(path))
+			{
+				connectionInfo.IsInitializedDbStructure = false;
+			}
+
+			var plat = new SQLitePlatformWinRT();
+			var connectionFactory = new Func<SQLiteConnectionWithLock>(() =>
+				new SQLiteConnectionWithLock(plat, new SQLiteConnectionString(path, storeDateTimeAsTicks: false)));
+			var asyncConnection = new SQLiteAsyncConnection(connectionFactory);
+			connectionInfo.Connection = asyncConnection;
+
+			return connectionInfo;
+		}
+
+		private async Task<bool> FileExists(string fileName)
+		{
+			var result = false;
+			try
+			{
+				var store = await ApplicationData.Current.LocalFolder.GetFilesAsync();
+				result = store.Any(x => Path.GetFileName(x.Name) == Path.GetFileName(fileName));
+			}
+			catch (Exception ex)
+			{
+			}
+
+			return result;
+		}
+	}
+}
